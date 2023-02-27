854e3f73750f655f845e8cbce6bd8997c46f6480
Christian.Starly <yoh.chrisitan@gmail.com>
2023-02-26 22:17:07 -0400

    Initial Commit

---
+++ QR-code-generator/QR_code_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_code_generatorApp.swift
+//  QR-code-generator
+//
+//  Created by Christian Calixto on 26/2/23.
+//
+
+import SwiftUI
+
+@main
+struct QR_code_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
